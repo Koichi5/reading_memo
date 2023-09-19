61db6242fda0057144fc629bbfd2310daea8e05d
Koichi5 <koichi20021217@gmail.com>
2023-09-19 17:42:30 +0900

    add cloud functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reading_memo/firebase_options.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +20,37 @@ void main() async {
   );
 }
 
+void helloWorld() async {
+  try {
+    final url =
+        "https://asia-northeast1-reading-memo-67bb8.cloudfunctions.net/testFunction";
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print('Response data: ${response.body}');
+    } else {
+      print(
+          'Failed to call cloud function. Status code: ${response.statusCode}');
+    }
+  } on FirebaseFunctionsException catch (e) {
+    log(e.code);
+    log(e.details ?? "null");
+    log(e.message ?? "message is null");
+  }
+}
+
+Future<void> callAddMessageFunction(String text) async {
+  final url =
+      'https://us-central1-reading-memo-67bb8.cloudfunctions.net/addMessage?text=$text';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    print('Response data: ${response.body}');
+  } else {
+    print('Failed to call cloud function. Status code: ${response.statusCode}');
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,53 +62,25 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text("サンプル")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            callAddMessageFunction("Your message text here");
+          },
+          child: const Text("say hello world"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
