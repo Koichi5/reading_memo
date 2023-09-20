455ffd55eaf0145b8c05ad3b74b3385e78a17285
Koichi5 <koichi20021217@gmail.com>
2023-09-20 18:01:16 +0900

    add pysummarization

--- functions/main.py
+++ functions/main.py
@@ -5,22 +5,88 @@
 from firebase_functions import firestore_fn, https_fn
 from firebase_admin import initialize_app, firestore
 import google.cloud.firestore
+import MeCab
+from sudachipy import Dictionary, tokenizer
 
 app = initialize_app()
 
 @https_fn.on_request()
-def addMessage(req: https_fn.Request) -> https_fn.Response:
-    original = req.args.get("text")
-    if original is None:
+def changeToMemoWithSudachi(req: https_fn.Request) -> https_fn.Response:
+    text = req.args.get("text")
+    if text is None:
         return https_fn.Response("No text parameter provided", status=400)
-    
     firestore_client = google.cloud.firestore.Client = firestore.client()
+    tokenizer_obj = Dictionary().create()
+    mode = tokenizer.Tokenizer.SplitMode.C  # モードCを指定
+    
+    words = [m.surface() for m in tokenizer_obj.tokenize(text, mode)]
+    
+    formatted_text = []
+    append_bullet = True  # 「・」を追加するかどうかのフラグ
+    for word in words:
+        # 「・」を追加する条件
+        if append_bullet and word not in ["って", "、", "。", "んだ"]:
+            formatted_text.append("・")
+            append_bullet = False
+            
+        # 「って」や「んだ」を除外する
+        if word in ["って", "んだ"]:
+            continue
+        
+        formatted_text.append(word)
+        
+        # 「って」や「は」や「が」の後、または句読点「、。」の前に改行を入れる
+        if word in ["は", "が", "、", "。"]:
+            formatted_text.append("\n")
+            append_bullet = True
+    
+    print("".join(formatted_text))
+    joined_text = "".join(formatted_text)
 
-    _, doc_ref = firestore_client.collection("messages").add(
-        {"original" : original}
-    )
+    _, doc_ref = firestore_client.collection("formatted_text").add({
+        "text" : joined_text
+    })
 
-    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+    return https_fn.Response(f"Memo with ID {doc_ref.id} added.")
+
+# @https_fn.on_request()
+# def changeToMemo(req: https_fn.Request) -> https_fn.Response:
+#     original = req.args.get("text")
+#     if original is None:
+#         return https_fn.Response("No text parameter provided", status=400)
+    
+#     firestore_client = google.cloud.firestore.Client = firestore.client()
+
+#     tagger = MeCab.Tagger()
+#     node = tagger.parseToNode(original)
+#     while node:
+#         surface = node.surface
+#         pos = node.feature[0]
+#         feature = node.feature
+#         print(f"{surface} ({pos}, {feature})")
+
+#     _, doc_ref = firestore_client.collection("memo_texts").add({
+#         "surface" : surface,
+#         "pos" : pos,
+#         "feature" : feature,
+#         "original" : original
+#     })
+    
+#     return https_fn.Response(f"Memo with ID {doc_ref.id} added.")
+
+# @https_fn.on_request()
+# def addMessage(req: https_fn.Request) -> https_fn.Response:
+#     original = req.args.get("text")
+#     if original is None:
+#         return https_fn.Response("No text parameter provided", status=400)
+    
+#     firestore_client = google.cloud.firestore.Client = firestore.client()
+
+#     _, doc_ref = firestore_client.collection("messages").add(
+#         {"original" : original}
+#     )
+
+#     return https_fn.Response(f"Message with ID {doc_ref.id} added.")
 
 # @firestore_fn.on_document_created(document="messages/{pushId}")
 # def makeuppercase(
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reading_memo/firebase_options.dart';
 import 'package:http/http.dart' as http;
 
@@ -22,14 +23,13 @@ void main() async {
 
 void helloWorld() async {
   try {
-    final url =
+    const url =
         "https://asia-northeast1-reading-memo-67bb8.cloudfunctions.net/testFunction";
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      print('Response data: ${response.body}');
+      log('Response data: ${response.body}');
     } else {
-      print(
-          'Failed to call cloud function. Status code: ${response.statusCode}');
+      log('Failed to call cloud function. Status code: ${response.statusCode}');
     }
   } on FirebaseFunctionsException catch (e) {
     log(e.code);
@@ -45,11 +45,37 @@ Future<void> callAddMessageFunction(String text) async {
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode == 200) {
-    print('Response data: ${response.body}');
+    log('Response data: ${response.body}');
   } else {
-    print('Failed to call cloud function. Status code: ${response.statusCode}');
+    log('Failed to call cloud function. Status code: ${response.statusCode}');
   }
 }
+
+Future<void> changeToMemo({required String text}) async {
+  final url =
+      "https://us-central1-reading-memo-67bb8.cloudfunctions.net/changeToMemo?text=$text";
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    log('Response data: ${response.body}');
+  } else {
+    log('Failed to call cloud function. Status code: ${response.statusCode}');
+  }
+}
+
+// final formattedTextProvider = StateProvider<String>((ref) => "");
+
+// Future<void> changeToMemoWithSudachi({required String text}) async {
+//   final url =
+//       "https://us-central1-reading-memo-67bb8.cloudfunctions.net/changeToMemoWithSudachi?text=$text";
+//   final response = await http.get(Uri.parse(url));
+//   print("response : $response");
+//   print("response.body : ${response.body}");
+//   if (response.statusCode == 200) {
+//     log('Response data: ${response.body}');
+//   } else {
+//     log('Failed to call cloud function. Status code: ${response.statusCode}');
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -67,19 +93,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+final formattedTextProvider = StateProvider<String>((ref) => "");
+
+class MyHomePage extends HookConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> changeToMemoWithSudachi({required String text}) async {
+      final url =
+          "https://us-central1-reading-memo-67bb8.cloudfunctions.net/changeToMemoWithSudachi?text=$text";
+      final response = await http.get(Uri.parse(url));
+      ref.watch(formattedTextProvider.notifier).state = response.body;
+      print("response : $response");
+      print("response.body : ${response.body}");
+      if (response.statusCode == 200) {
+        log('Response data: ${response.body}');
+      } else {
+        log('Failed to call cloud function. Status code: ${response.statusCode}');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text("サンプル")),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            callAddMessageFunction("Your message text here");
-          },
-          child: const Text("say hello world"),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                changeToMemoWithSudachi(text: "アインシュタインは相対性理論を発見して、ベロを出した天才科学者です。");
+              },
+              child: const Text("change to memo"),
+            ),
+            Text(ref.watch(formattedTextProvider.notifier).state),
+          ],
         ),
       ),
     );
